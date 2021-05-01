a8ba3095b135576c873c6873966483cd5385a667
Ali kareem <osama.kareem54@gmail.com>
2021-05-02 04:58:26 +0530

    Add project files.

---
+++ Summarising/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Summarising
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var text = "This is going to be really really really really really long text";
+            var summary = ProgramUtility.SummarizeText(text);
+            Console.WriteLine(summary);
+        }
+        
+    }
+}
---
+++ Summarising/ProgramUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summarising
+{
+    public class ProgramUtility
+    {
+        public static string SummarizeText(String text, int max_length = 20)
+        {
+            if (text.Length < max_length)
+            {
+                return text;
+            }
+            var words = text.Split(' ');
+            var totalCharacters = 0;
+
+            var summaryWords = new List<String>();
+
+            foreach (var word in words)
+            {
+                summaryWords.Add(word);
+                totalCharacters += word.Length + 1;
+                if (totalCharacters > max_length)
+                    break;
+            }
+            return String.Join(" ", summaryWords) + " ...";
+
+        }
+    }
+}
